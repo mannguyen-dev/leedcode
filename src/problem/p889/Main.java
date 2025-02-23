eff5ec3f677478484ee4cdb616356e2275e0471b
mannguyendev <mannguyen.dev@gmail.com>
2025-02-23 11:59:03 +0700

    Add problem 889

---
+++ src/problem/p889/Main.java
@@ -0,0 +1,30 @@
+package problem.p889;
+
+import problem.p1261.TreeNode;
+
+public class Main {
+    static int preIdx = 0;
+    static int postInx = 0;
+
+    public static void main(String[] args) {
+        int[] prerorder = new int[]{1,2,4,5,3,6,7};
+        int[] postorder = new int[]{4,5,2,6,7,3,1};
+        TreeNode tree = constructFromPrePost(prerorder, postorder);
+        TreeNode.inorderTraversal(tree);
+    }
+
+    public static TreeNode constructFromPrePost(int[] preorder, int[] postorder) {
+        TreeNode curr = new TreeNode(preorder[preIdx]);
+        preIdx++;
+
+        if (curr.val != postorder[postInx]){
+            curr.left = constructFromPrePost(preorder, postorder);
+        }
+
+        if (curr.val != postorder[postInx]){
+            curr.right = constructFromPrePost(preorder, postorder);
+        }
+        postInx++;
+        return curr;
+    }
+}
