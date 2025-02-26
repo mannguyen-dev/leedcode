fe6f132c6b81f12e2c9250c7d6e607b3e8693138
mannguyendev <mannguyen.dev@gmail.com>
2025-02-26 14:31:49 +0700

    Add problem

---
+++ src/problem/p3151/Main.java
@@ -0,0 +1,10 @@
+package problem.p3151;
+
+public class Main {
+    public boolean isArraySpecial(int[] nums) {
+        for (int i = 0; i < nums.length-1; i++){
+            if ((nums[i]&1) == (nums[i+1]&1) ) return false;
+        }
+        return true;
+    }
+}
