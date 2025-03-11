264477fb4b624025a1793fab858cb29cb5be1742
mannguyendev <mannguyen.dev@gmail.com>
2025-03-11 21:39:50 +0700

    Add problem 21 easy

---
+++ src/dsa/linkedlist/ListNode.java
@@ -0,0 +1,43 @@
+package dsa.linkedlist;
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    public void printList(){
+        ListNode cur = this;
+        while (cur != null){
+            System.out.print(cur.val + " ");
+            cur = cur.next;
+        }
+        System.out.println();
+    }
+
+    public static ListNode createLinkedList(int[] arr){
+        if (arr == null || arr.length == 0){
+            return null;
+        }
+
+        ListNode head = new ListNode(arr[0]);
+        ListNode current = head;
+
+        for (int i = 1; i < arr.length; i++){
+            current.next = new ListNode(arr[i]);
+            current = current.next;
+        }
+
+        return head;
+    }
+}
