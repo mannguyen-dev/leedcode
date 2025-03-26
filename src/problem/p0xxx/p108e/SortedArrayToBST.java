0fc82bd302c48c9217d69db92e16c86031a5ed2d
mannguyendev <mannguyen.dev@gmail.com>
2025-03-26 20:44:53 +0700

    Add Problem 108 easy

---
+++ src/problem/p0xxx/p108e/SortedArrayToBST.java
@@ -0,0 +1,26 @@
+package problem.p0xxx.p108e;
+
+import dsa.tree.TreeNode;
+
+import java.util.Arrays;
+
+public class SortedArrayToBST {
+    public static TreeNode sortedArrayToBST(int[] nums) {
+        return buildBST(nums, 0, nums.length - 1);
+    }
+
+    private static TreeNode buildBST(int[] nums, int left, int right) {
+        if (left > right) return null;
+
+        int mid = left + (right - left)/2;
+        TreeNode node = new TreeNode(nums[mid]);
+        node.left = buildBST(nums, left, mid - 1);
+        node.right = buildBST(nums,mid + 1, right);
+        return node;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-10,-3,0,5,9};
+        TreeNode.printLNR(sortedArrayToBST(nums));
+    }
+}
